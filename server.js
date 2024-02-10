8b86774d19226d097970fba1cfc8f4540715e029
Oscar Pozo <oscarpozo@uees.edu.ec>
2024-02-10 12:23:24 -0500

    Cambios en la recepción del audio de parte del cliente

--- server.js
+++ server.js
@@ -5,56 +5,117 @@ const fs = require('fs');
 const express = require('express');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
+require('dotenv').config()
+const Stack = require('./stack');
+const Queue = require('./queue');
+const { time } = require("console");
 
 
 const PORT = 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-const openai = new OpenAI({apiKey:"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"});
+const openai = new OpenAI({apikey:process.env.OPENAI_APIKEY});
+var stack = new Stack();
+var queue = new Queue();
 
-async function transcribirAudio (audio) {
-  const transcription = await openai.audio.transcriptions.create({
-    file: fs.createReadStream(audio),
-    model: "whisper-1",
-    language: "es",
-  });
+console.log("Check if the queue is empty")
+console.log(queue.isEmpty());
 
-  console.log(transcription.text);
-  return transcription;
-}
-transcribirAudio('audio.wav');
-
-//Prueba de servidor
-app.get('/prueba', function(req, res){
-  res.json({mensaje:'Hola Mundo'})
-});
-
-io.on('connection', (socket) => {
+io.on('connection',(socket) => {
   console.log('Cliente conectado');
 
-  // Manejar eventos de mensajes
-  socket.on('message', (data) => {
-    console.log('Mensaje recibido : '+ data);
-    // Emitir el mensaje a todos los clientes conectados
-    io.emit('message', 'Te saludos desde nodejs');
-  });
+  socket.on('start', (filename) => {
+    console.log('Grabación iniciada: ' + filename);
+    const audioStream = fs.createWriteStream(`${filename}.wav`)
+    //transcribe2 transcribe
+    //const transcriptionStream =  transcribe()
+    //const transcriptionStream =  transcribe(data)
 
-  socket.on('transcription',(data)=>{
-    // Convertir el buffer de audio recibido a formato compatible con Whisper
-    const audioBuffer = Buffer.from(data);
-    transcribirAudio(audioBuffer).then((result)=>{
-      //Envio de transcripcion
-      socket.emit('transcript', result.text);
+    socket.on('data',(data) => {
+      try {
+        // audioStream.write(data);
+        // transcriptionStream.write(data);
+        //stack.push(data);
+        setTimeout(() =>{
+          const audioBuffer = Buffer.from(data, 'base64');
+          // Guardar el archivo de audio
+          const filePath = `./audio/${Date.now()}.wav`;
+          fs.writeFile(filePath, audioBuffer, (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('Audio file saved successfully');
+            }
+          });
+          transcribe(filePath);  
+          if (data) {
+            console.log('Transcript: ' + filePath);
+            socket.emit('transcript', filePath);
+          } else{
+            console.log("No se recibió el audio");
+          }
+          // transcribe(data).on('data', (transcript) => {
+          //   if (transcript) {
+          //     console.log('Transcript: ' + transcript);
+          //     socket.emit('transcript', transcript);
+          //   } else{
+          //     console.log("No se recibió el audio");
+          //   }
+          // });
+        },15000);
+        //transcribe(data);
+        // queue.send(data);
+        // while (stack.length < 0){
+        //   transcribe(queue.receive);
+        // }
+        // transcribe(data).on('data', (transcript) => {
+        //   if (transcript) {
+        //     console.log('Transcript: ' + transcript);
+        //     socket.emit('transcript', transcript);
+        //   } else{
+        //     console.log("No se recibió el audio");
+        //   }
+        // });
+      } catch (error) {
+        console.log(error);
+        socket.emit('error: ',error.message);
+      }
+    })
+
+    socket.on('stop', () => {
+      console.log('Grabación finalizada');
+      audioStream.end();;
     });
   });
 
-  // Manejar evento de desconexión
   socket.on('disconnect', () => {
-    console.log('Cliente desconectado');
+    console.log('Client desconectado');
   });
 });
 
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
+
+async function transcribe(audio) {
+  try {
+    return await openai.audio.transcriptions.create({
+      model: "whisper-1",
+      language: "es",
+      file: audio
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// async function transcribe2() {
+//   return await openai.audio.transcriptions.create({
+//     model: "whisper-1",
+//     language: "es",
+//   });
+// }
+
+
+
